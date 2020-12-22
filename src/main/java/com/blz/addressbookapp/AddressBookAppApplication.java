7de610f5472c76288f2ba1bb6b9b698be2185b0b
Sampada-VS <samssv1@gmail.com>
2020-12-23 02:04:50 +0530

    Added: slf4j at class level and logging levels, patterns in application.properties

--- src/main/java/com/blz/addressbookapp/AddressBookAppApplication.java
+++ src/main/java/com/blz/addressbookapp/AddressBookAppApplication.java
@@ -2,13 +2,17 @@ package com.blz.addressbookapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import lombok.extern.slf4j.Slf4j;
 
 @SpringBootApplication
+@Slf4j
 public class AddressBookAppApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AddressBookAppApplication.class, args);
-		System.out.println("Welcome to Address Book Spring Project.");
+		ApplicationContext context=SpringApplication.run(AddressBookAppApplication.class, args);
+		log.info("Address Book app started in {} environment..",context.getEnvironment().getProperty("environment"));		
 	}
 
 }
